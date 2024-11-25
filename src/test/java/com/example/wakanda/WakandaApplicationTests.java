1cdd1476ae764f7d4e2fdea2fd3de1f6e3accf19
dxn1l <danielmorenorey2012@gmail.com>
2024-11-25 11:08:16 +0100

    Initial commit

---
+++ src/test/java/com/example/wakanda/WakandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wakanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
